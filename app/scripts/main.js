528526b8ae4c29d4b50af09b40f4eef554819066
Francisco Martinez Espartero <francisco.mar.esp@gmail.com>
2014-12-02 15:09:19 +0100

    galeria integrada

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,11 @@
 // File: app/scripts/main.js
 'use strict';
 (function() {
-	console.log('hola mundo!');
+	console.log('Aplicación iniciada!');
 })();
+
+function cambiaHistoria (img) {
+  $('#imagen_sumo').html("<img src='"+img.children[0].src+"' alt='"+img.children[0].dataset.nombre+"'>");
+  $('#nombre_sumo').html(img.children[0].dataset.nombre);
+  $('#descripcion_sumo').html(img.children[0].dataset.historia);
+}
